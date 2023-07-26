8fe20b7932db3428a8140bc6a700ed999735d365
Admin Mboa <adminmboa@Elvin-Ndichia.local>
2023-07-26 14:46:55 +0100

    Custom cursor now only shows on desktop

--- src/components/gpt/CursorCircle.jsx
+++ src/components/gpt/CursorCircle.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+import { Box } from "@mui/material";
 
 const CursorCircle = () => {
   const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
@@ -17,30 +18,33 @@ const CursorCircle = () => {
   }, []);
 
   return (
-    <div
-      className="cursor-circle"
-      style={{
-        transform: `translate3d(${cursorPos.x}px, ${cursorPos.y}px, 0) translate(-50%, -50%)`,
-        position: "fixed",
-
-        zIndex: 9999,
-        transition: "transform 0.15s linear",
-        mixBlendMode: "difference",
-      }}
-    >
+    <Box sx={{ display: { xs: "none", lg: "block" } }}>
+      {" "}
       <div
+        className="cursor-circle"
         style={{
-          position: "relative",
-          left: -30,
-          width: 20,
-          height: 20,
-          background: "#F84E32",
-          borderRadius: "50%",
-          pointerEvents: "none",
+          transform: `translate3d(${cursorPos.x}px, ${cursorPos.y}px, 0) translate(-50%, -50%)`,
+          position: "fixed",
+
+          zIndex: 9999,
+          transition: "transform 0.15s linear",
           mixBlendMode: "difference",
         }}
-      ></div>
-    </div>
+      >
+        <div
+          style={{
+            position: "relative",
+            left: -30,
+            width: 20,
+            height: 20,
+            background: "#F84E32",
+            borderRadius: "50%",
+            pointerEvents: "none",
+            mixBlendMode: "difference",
+          }}
+        ></div>
+      </div>
+    </Box>
   );
 };
 
